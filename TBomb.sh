73f40dc5f047d4b706a073beba8c2321df84ae9f
Ｇø∂ ✘ø <89073653+heaven2hellx@users.noreply.github.com>
2021-11-21 20:54:28 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -35,7 +35,17 @@ banner() {
         echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border SpeedX
+       ╔═══╦═══╦═══╗╔═╗╔═╦═══╗
+
+║╔═╗║╔═╗╠╗╔╗║╚╗╚╝╔╣╔═╗║
+
+║║─╚╣║─║║║║║║─╚╗╔╝║║─║║
+
+║║╔═╣║─║║║║║║─╔╝╚╗║║─║║
+
+║╚╩═║╚═╝╠╝╚╝║╔╝╔╗╚╣╚═╝║
+
+╚═══╩═══╩═══╝╚═╝╚═╩═══╝
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
     echo -e "\e[1;32m           Telegram: https://t.me/bi0xx_network \e[0m"
